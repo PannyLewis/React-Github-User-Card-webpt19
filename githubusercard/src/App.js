339f15006cbc21bf819ea248f6e88fb959b12fbe
Panny Lewis <pannylewis@gmail.com>
2020-09-08 20:53:43 -0500

    CRA, CDM in App.js, added 3 components: Cards is parent to Following and Users.

--- githubusercard/src/App.js
+++ githubusercard/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Users from "./components/Users";
+import Following from "./components/Following";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const App = () => {
+//   const [users, setUsers] = useState([]);
+//   const [following, setFollwing] = useState([]);
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      following: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://api.github.com/users/PannyLewis`)
+
+      .then((response) => {
+        console.log(response);
+
+        this.setState({
+          users: response.data,
+        });
+      })
+      .catch((err) => console.log(err));
+
+    axios
+      .get(`https://api.github.com/users/PannyLewis/following`)
+
+      .then((response) => {
+        console.log(response);
+
+        this.setState({
+          following: response.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Github Usercards</h1>
+        <Users users={this.state.users} />
+        {this.state.following.map((follow) => (
+          <Following key={follow.id} follow={follow} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ githubusercard/src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Users from "./Users";
+import Following from "./Following";
+
+function Card() {
+  return (
+    <div>
+      <Users />;
+      <Following />;
+    </div>
+  );
+}
+
+export default Card;
